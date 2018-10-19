61ec7770690864075e2007d0160796d8a2381b4d
huleilei <hull@huazhenit.com>
2018-10-19 14:47:49 +0800

    gateway

---
+++ code-gateway/src/main/java/club.codecloud.gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package club.codecloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author ulei
+ * @date 2018/4/12
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
