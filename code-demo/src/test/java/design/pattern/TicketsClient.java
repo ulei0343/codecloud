fdebb7f4d1b9941ab8a1b70158aa3671b7b5c653
huleilei <hull@huazhenit.com>
2018-11-30 22:05:46 +0800

    test

--- code-demo/src/test/java/design/pattern/TicketsClient.java
+++ code-demo/src/test/java/design/pattern/TicketsClient.java
@@ -18,4 +18,36 @@ public class TicketsClient {
         System.out.println("getInstance");
         return SingletonHolder.INSTANCE;
     }
+
+
+    public synchronized void test1() {
+        System.out.println("test1--" + Thread.currentThread().getName() + "--" + +System.currentTimeMillis() + this);
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void test2() {
+        synchronized (this) {
+            System.out.println("test2--" + Thread.currentThread().getName() + "--" + +System.currentTimeMillis());
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void test3() {
+        synchronized (this.getClass()) {
+            System.out.println("test3--" + Thread.currentThread().getName() + "--" + +System.currentTimeMillis() + this.getClass());
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
